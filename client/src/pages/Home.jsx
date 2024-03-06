ac26f5dd110b0b5aa2fba5acf3667a5051f663a5
NHK1905 <nguyenhuykhang195@gmail.com>
2024-03-06 23:00:22 +0700

    updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import UserMenu from '../components/UserMenu'
 import FolderList from '../components/FolderList'
 import { Outlet, useLoaderData } from 'react-router-dom'
+import PushNotification from '../components/PushNotification'
 
 export default function Home() {
   const { folders } = useLoaderData();
@@ -13,7 +14,8 @@ export default function Home() {
         Note App
       </Typography>
       <Box sx={{ display: 'flex', justifyContent: 'right', mb: '10px' }}>
-        <UserMenu></UserMenu>
+        <UserMenu />
+        <PushNotification />
       </Box>
       <Grid container sx={{ height: '50vh', boxShadow: '0 0 15px 0 rgb(193 193 193 /60%)'}}>
         <Grid item xs={3} sx={{ height: '100%' }}>
